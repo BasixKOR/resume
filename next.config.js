eb95ea9a224597995f44958e774e72c6d269b79a
Basix <basix@basix.tech>
2020-11-09 22:56:16 +0900

    feat(cms): add markdown loader

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    });
+    return cfg;
+  },
+};
